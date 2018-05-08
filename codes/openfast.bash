15c59e3df7c819ee2497bd3794c27701a54b0875
Shreyas Ananthan <shreyas@umd.edu>
2018-05-08 08:15:51 -0600

    Tweak OpenFAST build rules to work on Peregrine

--- codes/openfast.bash
+++ codes/openfast.bash
@@ -15,19 +15,20 @@ exawind_cmake ()
         install_dir="$(cd .. && pwd)/install"
     fi
 
-    local rpath_dirs="$(exawind_rpath_dirs zlib libxml2 hdf5 yaml-cpp):${install_dir}/lib"
-
     # Configure BLAS/LAPACK if user has setup the BLASLIB variable
     local blas_lapack=""
     if [ -n "$BLASLIB" ] ; then
         blas_lapack="-DBLAS_LIBRARIES=\"$BLASLIB\" -DLAPACK_LIBRARIES=\"$BLASLIB\""
     fi
 
+    # Force CMake to use absolute paths for the libraries so that it doesn't
+    # pick up versions installed in `/usr/lib64` on peregrine
+    local lib_path_save=${LIBRARY_PATH}
+    unset LIBRARY_PATH
+
     cmake \
         -DCMAKE_INSTALL_PREFIX:PATH=${install_dir} \
         -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-RELEASE} \
-        -DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=TRUE \
-        -DCMAKE_INSTALL_RPATH:STRING=${rpath_dirs} \
         -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS:-OFF} \
         -DFPE_TRAP_ENABLED:BOOL=ON \
         -DUSE_DLL_INTERFACE:BOOL=ON \
@@ -36,4 +37,6 @@ exawind_cmake ()
         -DHDF5_ROOT:PATH=${HDF5_ROOT_DIR} \
         ${blas_lapack} \
         ${extra_args} ..
+
+    export LIBRARY_PATH=${lib_path_save}
 }
