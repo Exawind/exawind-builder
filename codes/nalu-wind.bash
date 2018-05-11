78355157820fd63f73b41aba740577ab4e30df73
Shreyas Ananthan <shreyas@umd.edu>
2018-05-11 09:45:44 -0600

    Allow user to turn off nalu-wind deps

--- codes/nalu-wind.bash
+++ codes/nalu-wind.bash
@@ -18,11 +18,11 @@ exawind_cmake ()
         -DTrilinos_DIR:PATH=${TRILINOS_ROOT_DIR} \
         -DYAML_DIR:PATH=${YAML_CPP_ROOT_DIR} \
         -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-RELEASE} \
-        -DENABLE_HYPRE:BOOL=ON \
+        -DENABLE_HYPRE:BOOL=${ENABLE_HYPRE:-ON} \
         -DHYPRE_DIR:PATH=${HYPRE_ROOT_DIR} \
-        -DENABLE_TIOGA:BOOL=ON \
+        -DENABLE_TIOGA:BOOL=${ENABLE_TIOGA:-ON} \
         -DTIOGA_DIR:PATH=${TIOGA_ROOT_DIR} \
-        -DENABLE_OPENFAST:BOOL=ON \
+        -DENABLE_OPENFAST:BOOL=${ENABLE_OPENFAST:-ON} \
         -DOpenFAST_DIR:PATH=${OPENFAST_ROOT_DIR} \
         -DENABLE_TESTS:BOOL=ON \
         ${extra_args} .. 2>&1 | tee cmake_output.log
