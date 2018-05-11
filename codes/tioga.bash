1c11f8fc2ddc8bf6b66170b7d0418a4b466e508f
Shreyas Ananthan <shreyas@umd.edu>
2018-05-11 09:44:26 -0600

    Fix installation path for TIOGA

--- codes/tioga.bash
+++ codes/tioga.bash
@@ -10,7 +10,7 @@ exawind_cmake ()
     local extra_args="$@"
     local install_dir=""
     if [ -n "$TIOGA_INSTALL_PREFIX" ] ; then
-        install_dir="-DCMAKE_INSTALL_PREFIX=\"$TIOGA_INSTALL_PREFIX\""
+        install_dir="-DCMAKE_INSTALL_PREFIX=$TIOGA_INSTALL_PREFIX"
     fi
 
     command cmake ${install_dir} ${extra_args} ../
