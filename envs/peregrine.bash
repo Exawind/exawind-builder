79ee8d31f2b28286e90eb766aac30f3b7fb737c8
Shreyas Ananthan <shreyas@umd.edu>
2018-05-11 14:00:58 -0600

    Update Peregrine environment for CentOS7

--- envs/peregrine.bash
+++ envs/peregrine.bash
@@ -1,36 +1,39 @@
 #!/bin/bash
 
 export EXAWIND_NUM_JOBS=24
+export EXAWIND_MODULES_DIR=/nopt/nrel/ecom/ecp/base/c/spack/share/spack/modules/linux-centos7-x86_64/
+
+# Mapping identifying versions to load for each dependency
+declare -A EXAWIND_MODMAP
+EXAWIND_MODMAP[trilinos]=trilinos/develop-omp
+EXAWIND_MODMAP[openmpi]=openmpi/1.10.4
 
 exawind_env_gcc ()
 {
     module purge
-    module use /nopt/nrel/apps/modules/candidate/modulefiles/
+    module load gcc/6.2.0
+    module use ${EXAWIND_MODULES_DIR}/gcc-6.2.0
 
-    module load gcc/5.2.0
-    module unuse /nopt/nrel/ecom/ecp/base/modules/intel-17.0.2
-    module use /nopt/nrel/ecom/ecp/base/modules/gcc-5.2.0
+    module load binutils openmpi/1.10.4 netlib-lapack cmake
 
-    module load binutils openmpi netlib-lapack cmake
-
-    export CC=gcc
-    export CXX=g++
-    export FC=gfortran
+    export CC=$(which gcc)
+    export CXX=$(which g++)
+    export FC=$(which gfortran)
 }
 
 exawind_env_intel ()
 {
     module purge
-    module use /nopt/nrel/apps/modules/candidate/modulefiles/
-    module load comp-intel/2017.0.2
-    module unuse /nopt/nrel/ecom/ecp/base/modules/gcc-5.2.0
-    module use /nopt/nrel/ecom/ecp/base/modules/intel-17.0.2
+    module load gcc/6.2.0
+    module use ${EXAWIND_MODULES_DIR}/gcc-6.2.0
+    module load intel-parallel-studio/cluster.2018.1
+    module use ${EXAWIND_MODULES_DIR}/intel-18.1.163
 
     module load binutils intel-mpi intel-mkl cmake
 
-    export CC=icc
-    export CXX=icpc
-    export FC=ifort
+    export CC=$(which icc)
+    export CXX=$(which icpc)
+    export FC=$(which ifort)
 }
 
 exawind_load_deps ()
@@ -39,7 +42,7 @@ exawind_load_deps ()
         root_dir_var="$(echo $dep | sed -e 's/\([-a-zA-Z0-9_]*\).*/\1/;s/-/_/' | tr '[:lower:]' '[:upper:]')_ROOT_DIR"
 
         if [ -z ${!root_dir_var} ] ; then
-            module load $dep
+            module load ${EXAWIND_MODMAP[$dep]:-$dep}
         fi
     done
 }
