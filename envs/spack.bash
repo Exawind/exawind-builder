1661812faedb5abd28cfadaae9de6ae9228ba31b
Shreyas Ananthan <shreyas@umd.edu>
2018-07-02 12:04:14 -0600

    Update spack env for OSX

--- envs/spack.bash
+++ envs/spack.bash
@@ -7,24 +7,34 @@ exawind_spack_env ()
     export SPACK_ROOT=${SPACK_ROOT:-${HOME}/spack}
     export SPACK_EXE=${SPACK_ROOT}/bin/spack
 
+    if [[ $OSTYPE = "darwin"* ]] ; then
+        if [ -f /usr/local/opt/modules/init/bash ]; then
+            source /usr/local/opt/modules/init/bash
+        else
+            echo "ERROR: Cannot find module command. brew install modules"
+        fi
+    fi
     module use ${SPACK_ROOT}/share/spack/modules/$(${SPACK_EXE} arch)
 }
 
 exawind_env_gcc ()
 {
     export EXAWIND_COMPILER=${EXAWIND_COMPILER:-gcc}
+    export SPACK_COMPILER=${SPACK_COMPILER:-${EXAWIND_COMPILER}}
     exawind_spack_env
 }
 
 exawind_env_intel ()
 {
     export EXAWIND_COMPILER=${EXAWIND_COMPILER:-intel}
+    export SPACK_COMPILER=${SPACK_COMPILER:-${EXAWIND_COMPILER}}
     exawind_spack_env
 }
 
 exawind_env_clang ()
 {
     export EXAWIND_COMPILER=${EXAWIND_COMPILER:-clang}
+    export SPACK_COMPILER=${SPACK_COMPILER:-${EXAWIND_COMPILER}}
     exawind_spack_env
 }
 
@@ -35,8 +45,8 @@ exawind_load_deps ()
 
         local depname=${EXAWIND_MODMAP[$dep]:-$dep}
         if [ -z ${!root_dir_var} ] ; then
-            module load $(${SPACK_EXE} module find $depname %${EXAWIND_COMPILER})
-            eval "export $root_dir_var=$(${SPACK_EXE} location -i $depname %${EXAWIND_COMPILER})"
+            module load $(${SPACK_EXE} module find $depname %${SPACK_COMPILER})
+            eval "export $root_dir_var=$(${SPACK_EXE} location -i $depname %${SPACK_COMPILER})"
         fi
     done
 }
