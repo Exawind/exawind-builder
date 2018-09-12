54002a55760f27b02096009e4d88f9a084b1d1dc
Shreyas Ananthan <shreyas@umd.edu>
2018-09-12 11:05:46 -0600

    Add Ninja build capability
    - Introduce a new variable EXAWIND_MAKE_TYPE that can be used to control
      whether ninja or make is used for builds on Linux.
    
    - Requires the fortran-enabled fork of ninja at https://github.com/Kitware/ninja
    
    - Does some internal checks to see whether CMake or Ninja is used, that
      could be improved.

--- core.bash
+++ core.bash
@@ -59,25 +59,52 @@ exawind_load_deps ()
 exawind_cmake_full ()
 {
     set +e
-    rm -rf CMakeCache.txt CMakeFiles
+    rm -rf CMakeCache.txt CMakeFiles Makefile *.ninja
     set -e
-    exawind_cmake
+    exawind_cmake "$@"
 }
 
 exawind_cmake ()
 {
+    local cmake_option=" "
+
+    if [ ! -e "CMakeCache.txt" ] ; then
+        make_type=${EXAWIND_MAKE_TYPE:-make}
+        case ${make_type} in
+            "ninja")
+                cmake_option="-G Ninja"
+                ;;
+            "make")
+                cmake_option="-G 'Unix Makefiles'"
+                ;;
+            *)
+                echo "!!ERROR!! Unknown CMake generator provided: ${make_type}"
+                ;;
+        esac
+    fi
+
     if [ "$(uname)" = "Darwin" -a "$(type -t exawind_cmake_osx)" = "function" ] ; then
-        exawind_cmake_osx "$@"
+        exawind_cmake_osx "${cmake_option}" "$@"
     elif [ "$(type -t exawind_cmake_${EXAWIND_SYSTEM})" = "function" ] ; then
-        exawind_cmake_${EXAWIND_SYSTEM} "$@"
+        exawind_cmake_${EXAWIND_SYSTEM} "${cmake_option}" "$@"
     else
-        exawind_cmake_base "$@"
+        exawind_cmake_base "${cmake_option}" "$@"
+    fi
+}
+
+exawind_guess_make_type ()
+{
+    if [ -e "CMakeCache.txt" ] ; then
+        echo "$(awk -F '=' '/CMAKE_MAKE_PROGRAM:FILEPATH/ { print $2 }' CMakeCache.txt)"
+    else
+        echo "make"
     fi
 }
 
 exawind_make ()
 {
     local num_tasks=${EXAWIND_NUM_JOBS:-$EXAWIND_NUM_JOBS_DEFAULT}
+    local make_type=$(exawind_guess_make_type)
 
     if [ "$#" == "0" ] ; then
         extra_args="-j ${num_tasks}"
@@ -85,7 +112,17 @@ exawind_make ()
         extra_args="$@"
     fi
 
-    command make ${extra_args} 2>&1 | tee make_output.log
+    case ${make_type} in
+        *ninja)
+            command ${make_type} ${extra_args}
+            ;;
+        *make)
+            command ${make_type} ${extra_args} 2>&1 | tee make_output.log
+            ;;
+        *)
+            echo "!!ERROR!! Invalid make type detected"
+            exit 1
+    esac
 }
 
 exawind_ctest ()
