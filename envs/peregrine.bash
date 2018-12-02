da1a38ad03e66d7f5b982993a7e24f7469644a7f
Shreyas Ananthan <shreyas@umd.edu>
2018-12-02 14:25:22 -0700

    Update Peregrine and Rhodes environment
    Update environment to follow the new directory layout being implemented
    on these systems

--- envs/peregrine.bash
+++ envs/peregrine.bash
@@ -1,44 +1,49 @@
 #!/bin/bash
 
 export EXAWIND_NUM_JOBS_DEFAULT=24
-export EXAWIND_MODULES_DIR=/nopt/nrel/ecom/hpacf
 
 # Mapping identifying versions to load for each dependency
 declare -A EXAWIND_MODMAP
 EXAWIND_MODMAP[trilinos]=trilinos/develop
 
+exawind_peregrine_common ()
+{
+    local compiler_arg=$1
+
+    export EXAWIND_MODULES_DIR=/nopt/nrel/ecom/hpacf
+    local moddate=${EXAWIND_MODULES_SNAPSHOT:-modules}
+    module unuse /nopt/nrel/apps/modules/centos7/modulefiles
+    module use ${EXAWIND_MODULES_DIR}/compilers/${moddate}
+    module use ${EXAWIND_MODULES_DIR}/utilities/${moddate}
+    module use ${EXAWIND_MODULES_DIR}/software/${moddate}/${compiler_arg}
+
+    echo "==> Using modules: $(readlink -f ${EXAWIND_MODULES_DIR}/software/${moddate}/${compiler_arg})"
+}
+
 exawind_env_gcc ()
 {
     module purge
-    module unuse /nopt/nrel/apps/modules/centos7/modulefiles
-    module use ${EXAWIND_MODULES_DIR}/compilers/modules
-    module use ${EXAWIND_MODULES_DIR}/utilities/modules
-    module use ${EXAWIND_MODULES_DIR}/software/modules/gcc-7.3.0
+    exawind_peregrine_common gcc-7.3.0
 
+    module load gcc/7.3.0
     module load binutils openmpi netlib-lapack cmake
 
     export CC=$(which mpicc)
     export CXX=$(which mpicxx)
     export FC=$(which mpifort)
-
-    echo "==> Using modules: $(readlink -f ${EXAWIND_MODULES_DIR}/software/modules/gcc-7.3.0)"
 }
 
 exawind_env_intel ()
 {
     module purge
-    module unuse /nopt/nrel/apps/modules/centos7/modulefiles
-    module use ${EXAWIND_MODULES_DIR}/compilers/modules
-    module use ${EXAWIND_MODULES_DIR}/utilities/modules
-    module use ${EXAWIND_MODULES_DIR}/software/modules/intel-18.0.4
+    exawind_peregrine_common intel-18.0.4
 
+    module load intel-parallel-studio/cluster.2019.1
     module load binutils intel-mpi/2018.4.274 intel-mkl/2018.4.274 cmake
 
     export CC=$(which mpiicc)
     export CXX=$(which mpiicpc)
     export FC=$(which mpiifort)
-
-    echo "==> Using modules: $(readlink -f ${EXAWIND_MODULES_DIR}/software/modules/intel-18.0.4)"
 }
 
 exawind_load_deps ()
