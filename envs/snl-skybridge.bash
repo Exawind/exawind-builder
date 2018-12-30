8febf02e496b3637bd4c9e7db4a665525f38b80c
Shreyas Ananthan <shreyas@umd.edu>
2018-12-30 09:30:47 -0500

    Fix SNL Skybridge environment to be consistent with SNL Ghost
    Ensure consistent modules in both environments to allow using the same
    build on both systems.

--- envs/snl-skybridge.bash
+++ envs/snl-skybridge.bash
@@ -1,1 +1,44 @@
-snl-ghost.bash
+#!/bin/bash
+
+source ${__EXAWIND_CORE_DIR}/envs/spack.bash
+
+export EXAWIND_NUM_JOBS_DEFAULT=32
+
+exawind_env_common ()
+{
+    local compiler_env=$1
+
+    module purge
+    module load ${compiler_env}
+
+    # Disable use of Intel MKL because of linker issues 
+    export EXAWIND_USE_BLASLIB=OFF
+}
+
+exawind_env_intel ()
+{
+    exawind_env_common sierra-devel
+    exawind_spack_env intel
+    module unload gnu/4.9.2
+    module load gcc/4.9.3
+
+    export SPACK_COMPILER=intel
+    export CC=$(which mpicc)
+    export CXX=$(which mpicxx)
+    export F77=$(which mpifort)
+    export FC=$(which mpifort)
+
+    exawind_load_deps cmake netlib-lapack zlib libxml2
+}
+
+exawind_env_gcc ()
+{
+    echo "ERROR: No GCC environment setup for ${EXAWIND_SYSTEM}"
+    exit 1
+}
+
+exawind_env_clang ()
+{
+    echo "ERROR: No LLVM/Clang environment setup for ${EXAWIND_SYSTEM}"
+    exit 1
+}
--- etc/bootstrap/snl-skybridge.bash
+++ etc/bootstrap/snl-skybridge.bash
@@ -1,1 +1,10 @@
-snl-ghost.bash
+#!/bin/bash
+
+# Setup proxy to enable wget/curl downloads via http/https
+export http_proxy=http://wwwproxy.sandia.gov:80
+export https_proxy=https://wwwproxy.sandia.gov:80
+
+# Setup the environment for spack builds
+module load sierra-devel/intel
+module unload gnu/4.9.2
+module load gcc/4.9.3
