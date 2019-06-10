944dc939875535f58e36cb99da68baba05b22988
Shreyas Ananthan <shreyas@umd.edu>
2019-06-10 10:37:26 -0600

    Update Trilinos CMake STK config rules
    By default turn on all STK subpackages

--- codes/trilinos.bash
+++ codes/trilinos.bash
@@ -86,7 +86,6 @@ exawind_cmake_base ()
             -DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON
             -DTpetra_INST_DOUBLE:BOOL=ON
             -DTpetra_INST_INT_LONG:BOOL=ON
-            -DTpetra_INST_INT_LONG_LONG:BOOL=ON
             -DTpetra_INST_COMPLEX_DOUBLE:BOOL=OFF
             -DTrilinos_ENABLE_TESTS:BOOL=OFF
             -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF
@@ -110,17 +109,7 @@ exawind_cmake_base ()
             -DTrilinos_ENABLE_Ifpack:BOOL=OFF
             -DTrilinos_ENABLE_Amesos:BOOL=OFF
             -DTrilinos_ENABLE_Zoltan:BOOL=ON
-            -DTrilinos_ENABLE_STKMesh:BOOL=ON
-            -DTrilinos_ENABLE_STKSimd:BOOL=${enable_simd}
-            -DTrilinos_ENABLE_STKNGP:BOOL=ON
-            -DTrilinos_ENABLE_STKIO:BOOL=ON
-            -DTrilinos_ENABLE_STKTransfer:BOOL=ON
-            -DTrilinos_ENABLE_STKSearch:BOOL=ON
-            -DTrilinos_ENABLE_STKUtil:BOOL=ON
-            -DTrilinos_ENABLE_STKTopology:BOOL=ON
-            -DTrilinos_ENABLE_STKUnit_tests:BOOL=ON
-            -DTrilinos_ENABLE_STKUnit_test_utils:BOOL=ON
-            -DSTK_ENABLE_UnitMain:BOOL=OFF
+            -DTrilinos_ENABLE_STK:BOOL=ON
             -DTrilinos_ENABLE_Gtest:BOOL=ON
             -DTrilinos_ENABLE_STKClassic:BOOL=OFF
             -DTrilinos_ENABLE_STKExprEval:BOOL=ON
