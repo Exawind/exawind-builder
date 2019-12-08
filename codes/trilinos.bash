94d68387d0d973ef2bee0d28ba278cbba4bb4a42
Shreyas Ananthan <shreyas@umd.edu>
2019-12-08 15:50:11 -0700

    Fix deprecated Tpetra int_long instantiation

--- codes/trilinos.bash
+++ codes/trilinos.bash
@@ -91,7 +91,6 @@ exawind_cmake_base ()
             -DTrilinos_ENABLE_CXX11:BOOL=ON
             -DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON
             -DTpetra_INST_DOUBLE:BOOL=ON
-            -DTpetra_INST_INT_LONG:BOOL=ON
             -DTpetra_INST_COMPLEX_DOUBLE:BOOL=OFF
             -DTrilinos_ENABLE_TESTS:BOOL=OFF
             -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF
